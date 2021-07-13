d4f2aad41be9b244f4202caedb6217ab2e38fc19
Kesh West <kesh_west@hotmail.com>
2021-07-13 17:39:50 +1000

    Update user_input.py

--- user_input.py
+++ user_input.py
@@ -4,9 +4,6 @@ tails = int(input("Enter number of dovetails: "))
 spacing = int(input("Enter space between dovetails in mm: "))
 percentage = int(input("enter percentage of reduction/increase: "))
 
-percentage = percentage / 100
-
-
 #caculate pins total dimentions
 pins = tails + 1
 pins_total = pins * spacing
@@ -18,17 +15,17 @@ length = length - pins_total
 print("total length of pins", pins_total)
 print("remaining length for tails", length)
 
-
-#half piece
-tails = tails / 2
-length = length / tails
 even_tails = length / tails
-tails_up = length / 2
-tails_down = length / 2
+tails_up = even_tails
+tails_down = even_tails
 
 print(even_tails)
+tails = tails - 2 
 
-tails_up = tails_up + (tails_up / 100 * percentage) 
-tails_down = even_tails - (tails_down / 100 * percentage)
-print(tails_up)
-print(tails_down)
+while tails > 2:
+    tails = tails - 4
+    tails_up = tails_up + ((tails_up / 100 * percentage) * 2)
+    tails_down = tails_down - ((tails_down / 100 * percentage) * 2)
+    print(tails_up)
+    print(tails_down)
+    
